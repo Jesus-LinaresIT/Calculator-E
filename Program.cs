dadd409887ce9b8a4089c96d4954ef7e8e355a45
JesusLinares01 <34040095+JesusLinares01@users.noreply.github.com>
2017-12-14 14:44:14 -0600

    Calculadora estandar

--- Program.cs
+++ Program.cs
@@ -1,21 +1,16 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Calculadora
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
             string Seguir;
             int operacion;
 
             do
             {
-
                 Console.WriteLine("Escriba la opcion que desee realizar: ");
 
                 Console.WriteLine("1-Sumar");
@@ -37,113 +32,94 @@ namespace Calculadora
                 Seguir = Console.ReadLine();
 
                 if (Seguir == "1" || Seguir == "Si")
-                {
                     Console.Clear();
-
-                }
-
             } while (Seguir == "1" || Seguir == "Si");
 
             Console.WriteLine("Programa terminado, presione cualquier tecla para salir...");
             Console.ReadKey();
-
         }
 
         private static void Calcular(int operacion)
         {
-
             Console.Clear();
             switch (operacion)
             {
-
                 case 1:
-                    {
-                        Console.WriteLine("SUMAR");
-                        Console.WriteLine(" ");
-                        CalcularResult(operacion);
-                        break;
-                    }
+                {
+                    Console.WriteLine("SUMAR");
+                    Console.WriteLine(" ");
+                    CalcularResult(operacion);
+                    break;
+                }
 
                 case 2:
-                    {
-                        Console.WriteLine("RESTA");
-                        Console.WriteLine(" ");
-                        CalcularResult(operacion);
-                        break;
-                    }
+                {
+                    Console.WriteLine("RESTA");
+                    Console.WriteLine(" ");
+                    CalcularResult(operacion);
+                    break;
+                }
 
                 case 3:
-                    {
-                        Console.WriteLine("MULTIPLICACION");
-                        Console.WriteLine(" ");
-                        CalcularResult(operacion);
-                        break;
-                    }
+                {
+                    Console.WriteLine("MULTIPLICACION");
+                    Console.WriteLine(" ");
+                    CalcularResult(operacion);
+                    break;
+                }
 
                 case 4:
-                    {
-                        Console.WriteLine("DIVISION");
-                        Console.WriteLine(" ");
-                        CalcularResult(operacion);
-                        break;
-
-                    }
+                {
+                    Console.WriteLine("DIVISION");
+                    Console.WriteLine(" ");
+                    CalcularResult(operacion);
+                    break;
+                }
 
                 default:
                     break;
-
             }
-
         }
 
         private static void CalcularResult(int operacion)
         {
-
             Console.WriteLine("Escriba el primer Numero: ");
-            Double Numero1 = Double.Parse(Console.ReadLine());
+            var numero1 = double.Parse(Console.ReadLine());
             Console.WriteLine("Escriba el primer Segundo: ");
-            Double Numero2 = Double.Parse(Console.ReadLine());
+            var numero2 = double.Parse(Console.ReadLine());
 
             if (operacion == 1)
             {
-
-                double resultado = Numero1 + Numero2;
-                Console.WriteLine("Resultado {0} + {1} = {2}", Numero1, Numero2, resultado);
+                var resultado = numero1 + numero2;
+                Console.WriteLine("Resultado {0} + {1} = {2}", numero1, numero2, resultado);
             }
 
 
             if (operacion == 2)
             {
-
-                double resultado = Numero1 - Numero2;
-                Console.WriteLine("Resultado {0} - {1} = {2}", Numero1, Numero2, resultado);
+                var resultado = numero1 - numero2;
+                Console.WriteLine("Resultado {0} - {1} = {2}", numero1, numero2, resultado);
             }
 
             if (operacion == 3)
             {
-
-                double resultado = Numero1 * Numero2;
-                Console.WriteLine("Resultado {0} x {1} = {2}", Numero1, Numero2, resultado);
+                var resultado = numero1 * numero2;
+                Console.WriteLine("Resultado {0} x {1} = {2}", numero1, numero2, resultado);
             }
 
 
             if (operacion == 4)
-            {
-                if (Numero2 != 0)
+                if (numero2 != 0)
                 {
-
-                    double resultado = Numero1 / Numero2;
-                    Console.WriteLine("Resultado {0} / {1} = {2}", Numero1, Numero2, resultado);
+                    var resultado = numero1 / numero2;
+                    Console.WriteLine("Resultado {0} / {1} = {2}", numero1, numero2, resultado);
                 }
                 else
                 {
-
-                    Console.WriteLine("No se puede dividir entre {0}", Numero2);
+                    Console.WriteLine("No se puede dividir entre {0}", numero2);
                 }
-            }
 
             Console.WriteLine(" ");
         }
-
     }
 }
